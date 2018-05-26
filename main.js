650aacbca0c927f81f243800471abe80923630c2
sighowe <sig@howeville.com>
2018-05-26 10:11:22 -0500

    v1.0 Pre-Alpha

---
+++ main.js
@@ -0,0 +1,24 @@
+var Reset = function(){
+  var Mango = 0;
+  var MangoInt= 1;
+  var MangoesPerSec = 0;
+}
+var AddMango = function(){
+  Mango = Mango + MangoInt;
+  localStorage.setItem("Mango", Mango);
+  document.getElementById("MangoLabel").textContent = "You Have " + Mango + " Mangoes";
+}
+if(localStorage.getItem("save") == 1){
+  Reset();
+  Mango = localStorage.getItem("Mango")
+  MangoInt = localStorage.getItem("MangoInt");
+  MangoesPerSec = localStorage.getItem("MangoesPerSec");
+  window.alert("Save Loaded!");
+}
+else{
+  Reset();
+  localStorage.setItem("save", 1);
+  localStorage.setItem("Mango", 0);
+  localStorage.setItem("MangoesPerSec", 0);
+  localStorage.setItem("MangoInt", 1);
+}
