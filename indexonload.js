97629b8bb5457118811d1d41dcebb896416cdc0a
sighowe <sig@howeville.com>
2018-05-26 10:32:12 -0500

    v1.06 Pre-Alpha

--- indexonload.js
+++ indexonload.js
@@ -1,5 +1,5 @@
 Reset();
-Mango = localStorage.getItem("Mango")
+Mango = localStorage.getItem("Mango");
 MangoInt = localStorage.getItem("MangoInt");
 MangoesPerSec = localStorage.getItem("MangoesPerSec");
 document.getElementById("MangoLabel").textContent = "You Have " + Mango + " Mangoes";
