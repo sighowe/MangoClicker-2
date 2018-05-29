3d167baad0aee4d6ae083d61e74b31ec9a4e87a8
Sigmund Howe <sig@howeville.com>
2018-05-29 13:00:29 -0500

    v1.33 Pre-Alpha

--- indexonload.js
+++ indexonload.js
@@ -3,3 +3,4 @@ Mango = localStorage.getItem("Mango");
 MangoInt = localStorage.getItem("MangoInt");
 MangoesPerSec = localStorage.getItem("MangoesPerSec");
 document.getElementById("MangoLabel").textContent = "You Have " + Mango + " Mangoes";
+
--- main.js
+++ main.js
@@ -1,18 +1,31 @@
-var Reset = function(){
-  var Mango = 0;
-  var MangoInt = 1;
-  var MangoesPerSec = 0;
+
+var Mango = 0;
+  
+var MangoInt = 1;
+  
+var MangoesPerSec = 0;
+
 }
+
 var TrueReset = function(){
-  var Mango = 0;
-  var MangoInt = 1;
-  var MangoesPerSec = 0;
-  localStorage.setItem("save", 1);
-  localStorage.setItem("Mango", 0);
-  localStorage.setItem("MangoesPerSec", 0);
-  localStorage.setItem("MangoInt", 1);
-  document.getElementById("MangoLabel").textContent = "You have " + Mango + " Mangoes";
-  Reset();
+  
+var Mango = 0;
+  
+var MangoInt = 1;
+  
+var MangoesPerSec = 0;
+  
+localStorage.setItem("save", 1);
+  
+localStorage.setItem("Mango", 0);
+  
+localStorage.setItem("MangoesPerSec", 0);
+  
+localStorage.setItem("MangoInt", 1);
+  
+document.getElementById("MangoLabel").textContent = "You have " + Mango + " Mangoes";
+  
+Reset();
   Mango = localStorage.getItem("Mango")
   MangoInt = localStorage.getItem("MangoInt");
   MangoesPerSec = localStorage.getItem("MangoesPerSec");
@@ -30,6 +43,13 @@ if(localStorage.getItem("save") == 1){
   MangoInt = localStorage.getItem("MangoInt");
   MangoesPerSec = localStorage.getItem("MangoesPerSec");
   document.getElementById("MangoLabel").textContent = "You have " + Mango + " Mangoes";
+  Reset();
+ Mango = localStorage.getItem("Mango");
+ MangoInt = localStorage.getItem("MangoInt");
+ MangoesPerSec = localStorage.getItem("MangoesPerSec");
+ document.getElementById("MangoLabel").textContent = "You Have " + Mango + " Mangoes";
+
+
 }
 else{
   Reset();
