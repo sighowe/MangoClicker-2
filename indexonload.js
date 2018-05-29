d6a5404b4ee3aa0d8b4f292c8eb14018c55aaf1c
Sigmund Howe <sig@howeville.com>
2018-05-29 12:33:42 -0500

    v2.0 Beta
    *bug fixes

--- indexonload.js
+++ indexonload.js
@@ -1,5 +1,6 @@
 Reset();
-Mango = localStorage.getItem("Mango");
-MangoInt = localStorage.getItem("MangoInt");
-MangoesPerSec = localStorage.getItem("MangoesPerSec");
+document.getElementById("MangoLabel").textContent = "You Have " + Mango + " Mangoes";
+Mango = +localStorage.getItem("Mango");
+MangoInt = +localStorage.getItem("MangoInt");
+MangoesPerSec = +localStorage.getItem("MangoesPerSec");
 document.getElementById("MangoLabel").textContent = "You Have " + Mango + " Mangoes";
