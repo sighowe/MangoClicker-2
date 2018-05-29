e11e0b6ec19c77ca93fbfa95a060ba8c04f53248
Sigmund Howe <sig@howeville.com>
2018-05-29 16:06:03 -0500

    v2.08 Pre-Alpha

--- AddMango.js
+++ AddMango.js
@@ -1,5 +1,9 @@
+  Mango = localStorage.getItem("Mango")
+  MangoInt = localStorage.getItem("MangoInt");
+  MangoesPerSec = localStorage.getItem("MangoesPerSec");
 var AddMango = function(){
   Mango = +MangoInt + +Mango;
   localStorage.setItem("Mango", Mango);
   document.getElementById("MangoLabel").textContent = "You have " + Mango + " Mangoes";
 }
+
