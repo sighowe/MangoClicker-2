891b6f8455e848e2350d70922ec8a2ee43aad7be
Sigmund Howe <sig@howeville.com>
2018-05-30 15:13:02 -0500

    4.0

--- MangoLabel.js
+++ MangoLabel.js
@@ -1,8 +1,13 @@
 var Mango = localStorage.getItem("Mango");
+var MangoInt = localStorage.getItem("MangoInt");
+
 
 window.setInterval(function(){
 
 
-Mango = localStorage.getItem("Mango");  
-document.getElementById("MangoLabel").textContent = "You have " + Mango + " Mangoes";
+Mango = localStorage.getItem("Mango");
+MangoInt = localStorage.getItem("MangoInt");  
+document.getElementById("MangoLabel").textContent = "You have " + Mango + " Mangoes" + "   ( " + MangoInt + " MPS)";
+
+
 }, 50);
