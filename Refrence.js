75ce65d0d1f78a6a5171f4521e0ed71bff9760ac
Sigmund Howe <sig@howeville.com>
2018-05-30 12:47:50 -0500

    v3.1 Pre-Alpha

---
+++ Refrence.js
@@ -0,0 +1,31 @@
+var Reset = function(){
+
+var Mango = 0;
+  
+var MangoInt = 1;
+  
+var MangoesPerSec = 0;
+
+}
+
+var TrueReset = function(){
+  var Mango = 0;
+  var MangoInt = 1;
+  var MangoesPerSec = 0;
+  localStorage.setItem("save", 1);
+  localStorage.setItem("Mango", 0);
+  localStorage.setItem("MangoesPerSec", 0);
+  localStorage.setItem("MangoInt", 1);
+  document.getElementById("MangoLabel").textContent = "You have " + Mango + " Mangoes";
+  Reset();
+  Mango = localStorage.getItem("Mango")
+  MangoInt = localStorage.getItem("MangoInt");
+  MangoesPerSec = localStorage.getItem("MangoesPerSec");
+  document.getElementById("MangoLabel").textContent = "You have " + Mango + " Mangoes";
+  location.reload();
+}
+var AddMango = function(){
+  Mango = +MangoInt + +Mango;
+  localStorage.setItem("Mango", Mango);
+  document.getElementById("MangoLabel").textContent = "You have " + Mango + " Mangoes";
+}
